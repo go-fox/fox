0bcc32ebea77c1bbb58e1702427f8aaed8be0f35
liuqin <15023989265@163.com>
2025-02-22 15:26:10 +0800

    feat:json解析bug

--- codec/json/json.go
+++ codec/json/json.go
@@ -24,15 +24,29 @@
 package json
 
 import (
-	"bytes"
 	"encoding/json"
+	"reflect"
 	"sync"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 
 	"github.com/go-fox/fox/codec"
 )
 
 // Name json编解码器名称
 const Name = "json"
+
+var (
+	// MarshalOptions is a configurable JSON format marshaller.
+	MarshalOptions = protojson.MarshalOptions{
+		EmitUnpopulated: true,
+	}
+	// UnmarshalOptions is a configurable JSON format parser.
+	UnmarshalOptions = protojson.UnmarshalOptions{
+		DiscardUnknown: true,
+	}
+)
 
 var initOnce sync.Once
 
@@ -59,8 +73,10 @@ func (Codec) Marshal(v interface{}) ([]byte, error) {
 	switch m := v.(type) {
 	case json.Marshaler:
 		return m.MarshalJSON()
+	case proto.Message:
+		return MarshalOptions.Marshal(m)
 	default:
-		return json.Marshal(v)
+		return json.Marshal(m)
 	}
 }
 
@@ -69,9 +85,19 @@ func (Codec) Unmarshal(data []byte, v interface{}) error {
 	switch m := v.(type) {
 	case json.Unmarshaler:
 		return m.UnmarshalJSON(data)
+	case proto.Message:
+		return UnmarshalOptions.Unmarshal(data, m)
 	default:
-		d := json.NewDecoder(bytes.NewReader(data))
-		d.UseNumber()
-		return d.Decode(m)
+		rv := reflect.ValueOf(v)
+		for rv := rv; rv.Kind() == reflect.Ptr; {
+			if rv.IsNil() {
+				rv.Set(reflect.New(rv.Type().Elem()))
+			}
+			rv = rv.Elem()
+		}
+		if m, ok := reflect.Indirect(rv).Interface().(proto.Message); ok {
+			return UnmarshalOptions.Unmarshal(data, m)
+		}
+		return json.Unmarshal(data, m)
 	}
 }
