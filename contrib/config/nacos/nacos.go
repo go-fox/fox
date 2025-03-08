bd78f5c735185fe85a5d52fe553966d13b837812
RuffianLiu <876771120@qq.com>
2025-03-08 21:30:57 +0800

    feat：更新依赖,增加nacos配置中心

--- contrib/config/nacos/nacos.go
+++ contrib/config/nacos/nacos.go
@@ -3,33 +3,54 @@ package nacos
 import (
 	"context"
 	"github.com/go-fox/fox/config"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
 	configClient "github.com/nacos-group/nacos-sdk-go/v2/clients/config_client"
-	nacosClient "github.com/nacos-group/nacos-sdk-go/v2/clients/nacos_client"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/file"
 	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"os"
 	"path/filepath"
 	"strings"
 	"time"
 )
 
 type nacos struct {
-	opts   *vo.ConfigParam
+	opts   *options
 	client configClient.IConfigClient
 }
 
 // NewSource  构造函数
-func NewSource(nacosClient nacosClient.INacosClient, opts ...Option) config.Source {
-	client, err := configClient.NewConfigClient(
-		nacosClient,
+func NewSource(opts ...Option) config.Source {
+	// 创建clientConfig
+	o := &options{
+		clientConfig: constant.ClientConfig{
+			TimeoutMs:            10 * 1000,
+			BeatInterval:         5 * 1000,
+			OpenKMS:              false,
+			CacheDir:             file.GetCurrentPath() + string(os.PathSeparator) + "cache",
+			UpdateThreadNum:      20,
+			NotLoadCacheAtStart:  false,
+			UpdateCacheWhenEmpty: false,
+			LogDir:               file.GetCurrentPath() + string(os.PathSeparator) + "log",
+			LogLevel:             "info",
+		},
+		configParam:   vo.ConfigParam{},
+		serverConfigs: make([]constant.ServerConfig, 0),
+	}
+	for _, opt := range opts {
+		opt(o)
+	}
+	client, err := clients.NewConfigClient(
+		vo.NacosClientParam{
+			ClientConfig:  &o.clientConfig,
+			ServerConfigs: o.serverConfigs,
+		},
 	)
 	if err != nil {
 		panic(err)
 	}
-	options := &vo.ConfigParam{}
-	for _, opt := range opts {
-		opt(options)
-	}
 	return &nacos{
-		opts:   options,
+		opts:   o,
 		client: client,
 	}
 }
@@ -37,13 +58,13 @@ func NewSource(nacosClient nacosClient.INacosClient, opts ...Option) config.Sour
 // Load 加载数据
 func (n *nacos) Load() ([]*config.DataSet, error) {
 	content, err := n.client.GetConfig(vo.ConfigParam{
-		DataId: n.opts.DataId,
-		Group:  n.opts.Group,
+		DataId: n.opts.configParam.DataId,
+		Group:  n.opts.configParam.Group,
 	})
 	if err != nil {
 		return nil, err
 	}
-	k := n.opts.DataId
+	k := n.opts.configParam.DataId
 	return []*config.DataSet{
 		{
 			Key:       k,
@@ -56,10 +77,10 @@ func (n *nacos) Load() ([]*config.DataSet, error) {
 
 // Watch 开启监控
 func (n *nacos) Watch() (config.Watcher, error) {
-	watcher := newWatcher(context.Background(), n.opts.DataId, n.opts.Group, n.client.CancelListenConfig)
+	watcher := newWatcher(context.Background(), n.opts.configParam.DataId, n.opts.configParam.Group, n.client.CancelListenConfig)
 	err := n.client.ListenConfig(vo.ConfigParam{
-		DataId: n.opts.DataId,
-		Group:  n.opts.Group,
+		DataId: n.opts.configParam.DataId,
+		Group:  n.opts.configParam.Group,
 		OnChange: func(_, group, dataId, data string) {
 			if dataId == watcher.dataID && group == watcher.group {
 				watcher.content <- data
--- contrib/config/nacos/options.go
+++ contrib/config/nacos/options.go
@@ -1,21 +1,90 @@
 package nacos
 
 import (
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
 	"github.com/nacos-group/nacos-sdk-go/v2/vo"
+	"net/url"
+	"strconv"
 )
 
-type Option func(o *vo.ConfigParam)
+type options struct {
+	clientConfig  constant.ClientConfig
+	serverConfigs []constant.ServerConfig
+	configParam   vo.ConfigParam
+}
 
-// WithDataID 设置数据ID
-func WithDataID(dataID string) Option {
-	return func(o *vo.ConfigParam) {
-		o.DataId = dataID
+// Option 参数
+type Option func(o *options)
+
+// WithServer 设置nacos服务地址
+func WithServer(url url.URL) Option {
+	portStr := url.Port()
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		port = 80
+	}
+	return func(o *options) {
+		o.serverConfigs = append(o.serverConfigs, constant.ServerConfig{
+			Scheme:      url.Scheme,
+			ContextPath: url.Path,
+			Port:        uint64(port),
+			IpAddr:      url.Host,
+		})
+	}
+}
+
+// WithNamespaceID 设置namespace
+func WithNamespaceID(namespaceID string) Option {
+	return func(o *options) {
+		o.clientConfig.NamespaceId = namespaceID
+	}
+}
+
+// WithTimeoutMs 设置超时时间
+func WithTimeoutMs(timeout uint64) Option {
+	return func(o *options) {
+		o.clientConfig.TimeoutMs = timeout
+	}
+}
+
+// WithNotLoadCacheAtStart 启动时不加载缓存
+func WithNotLoadCacheAtStart() Option {
+	return func(o *options) {
+		o.clientConfig.NotLoadCacheAtStart = true
+	}
+}
+
+// WithLogDir 日志目录
+func WithLogDir(dir string) Option {
+	return func(o *options) {
+		o.clientConfig.LogDir = dir
+	}
+}
+
+// WithCacheDir 缓存目录
+func WithCacheDir(dir string) Option {
+	return func(o *options) {
+		o.clientConfig.CacheDir = dir
+	}
+}
+
+// WithLogLevel 设置日志等级
+func WithLogLevel(dir string) Option {
+	return func(o *options) {
+		o.clientConfig.LogLevel = dir
 	}
 }
 
 // WithGroup 设置分组
 func WithGroup(group string) Option {
-	return func(o *vo.ConfigParam) {
-		o.Group = group
+	return func(o *options) {
+		o.configParam.Group = group
+	}
+}
+
+// WithDataID 设置配置ID
+func WithDataID(dataID string) Option {
+	return func(o *options) {
+		o.configParam.DataId = dataID
 	}
 }
